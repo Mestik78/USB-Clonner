7315b005719f15ee949b40790755b5c3f99287af
Mestik78 <mestik78@gmail.com>
2022-02-06 20:16:36 +0100

    Clonner

---
+++ clonner.bat
@@ -0,0 +1,14 @@
+@echo off
+:start
+
+for /F "tokens=1*" %%a in ('fsutil fsinfo drives') do (
+   for %%c in (%%b) do (
+      for /F "tokens=3" %%d in ('fsutil fsinfo drivetype %%c') do (
+         if %%d equ Removable (            
+            xcopy %%c\ C:\copies\%date:~7,2%-%date:~3,2%-%date:~10,4%-%time:~0,2%-%time:~3,2%\ /E
+         )
+      )
+   )
+)
+timeout 1
+goto start
