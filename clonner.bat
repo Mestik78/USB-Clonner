3a3d34166c1bdaac7dabfaf2886faf9f648d43f5
Mestik78 <mestik78@gmail.com>
2022-02-07 23:50:12 +0100

    Update clonner.bat

--- clonner.bat
+++ clonner.bat
@@ -1,14 +1,22 @@
 @echo off
-:start
+:usb-not-connected
 
 for /F "tokens=1*" %%a in ('fsutil fsinfo drives') do (
    for %%c in (%%b) do (
       for /F "tokens=3" %%d in ('fsutil fsinfo drivetype %%c') do (
          if %%d equ Removable (            
-            xcopy %%c\ "C:\Users\USER\copies\%date:~7,2%-%date:~3,2%-%date:~10,4%-%time:~0,2%-%time:~3,2%\" /E /Y
+            xcopy %%c\ "C:\Users\Usuario\copies\%date:~7,2%-%date:~3,2%-%date:~10,4%-%time:~0,2%-%time:~3,2%\" /E /Y
+            
+            
+            :usb-connected
+            timeout 1
+            if %%d equ Removable (          
+               goto usb-connected
+            )
          )
       )
    )
 )
+
 timeout 1
-goto start
+goto usb-not-connected
